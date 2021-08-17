7819ff12f2577d07aa8b9f59c77308283c0f3864
Binh Do <binhdq@fe.edu.vn>
2021-08-17 10:36:46 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.end("Hello")
+})
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT)
+console.log('running...')
